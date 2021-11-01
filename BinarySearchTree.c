14ab43e272d619da6a3eb3cb41f61f31c644f497
sekar2002 <91018034+sekar2002@users.noreply.github.com>
2021-11-01 19:30:20 +0530

    update delete,maximum element,display

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -27,8 +27,61 @@ struct node *insert(int x, struct node*t)
       t->right=insert(x,t->right);
       return t;
 } 
-void main()
+//find max value
+int findmax(struct node*T)
 {
+    if(T==NULL)
+      return-1;
+    while(T->right!=NULL)
+      T=T->right;
+      return(T->data);
+}
+//delete
+struct node*delete_n(struct node*T,int x)
+
+{
+    
+    if(T==NULL) 
+     return NULL;
+  if(x<T->data)
+   T->left=delete_n(T->left,x);
+  else if(x>T->data)
+   T->right=delete_n(T->right,x);//traverse nodes
+  else 
+  {
+      if ((T->left==NULL) && (T->right==NULL))
+        return NULL;
+      if((T->left==NULL) && (T->right!=NULL))
+        return(T->right);
+      if((T->right==NULL) && (T->left!=NULL))
+        return(T->left);
+      else
+      {
+      struct node*Q;
+         Q=T->right;
+        while(Q->left!=NULL)
+         Q=Q->left;
+       
+        T->right=delete_n(T->right,Q->data);
+      }
+      
+           
+  }
+}   
+// display using inorder
+void inorder(struct node*t1)
+{
+    if(t1==NULL)
+    return;
+    else
+    {
+        inorder(t1->left);
+        printf(" %d ",t1->data);
+        inorder(t1->right);
+    }
+}
+void main()
+{    int y;
 	struct node*root=NULL;
     root=insert(52,root);
     root=insert(38,root);
@@ -38,4 +91,9 @@ void main()
     root=insert(55,root);
     root=insert(34,root);
     root=insert(35,root);
+    inorder(root);
+     y=findmax(root);
+    printf("\n %d",y);
+    delete_n(root,38);
+    inorder(root);
 }
